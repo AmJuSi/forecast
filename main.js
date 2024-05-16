9b865eda04f5a35a62cdc31c127084e7ddad8069
AmJuSi <Amelie.Singler@student.uibk.ac.at>
2024-05-16 16:27:29 +0200

    Wetter icons für die nächsten 24 stunden

--- main.js
+++ main.js
@@ -57,6 +57,7 @@ async function showForecast(url) {
 
             for (let i = 0; i <= 24; i += 3) {
                 let symbol = feature.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+                content += `<img src="icons/${symbol}.svg" alt="${symbol}" style="width:32px">`;
             }
 
 
